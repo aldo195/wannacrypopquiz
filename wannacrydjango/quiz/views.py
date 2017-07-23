2e8926a9760097531b51d33be3f7d7da7cc49d75
aldo195 <osinger@gmail.com>
2017-07-23 09:10:37 -0700

    first view under construction

--- wannacrydjango/quiz/views.py
+++ wannacrydjango/quiz/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('quiz/index.html')
+    context = {
+        'latest_question_list': "1",
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def results(request, question_id):
